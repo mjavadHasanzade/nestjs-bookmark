b8e5d58dad8ce8ca9a8985f83b93d8616d4bb4b3
mjavadHasanzade <hjavad522@gmail.com>
2024-06-16 17:04:40 +0330

    feat: handle save user in database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -11,9 +12,14 @@ export class AuthService {
       msg: ' I have signed in',
     };
   }
-  signup(dto: AuthDto) {
-    return {
-      msg: ' I have signed up',
-    };
+
+  async signup(dto: AuthDto) {
+    const hash = await argon2.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: { email: dto.email, hash },
+    });
+
+    return user;
   }
 }
