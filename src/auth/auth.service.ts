e7dfff7240f4c140d128e1f6daa9f90b8bce6a3f
mjavadHasanzade <hjavad522@gmail.com>
2024-06-16 16:27:08 +0330

    feat: add DTO handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +11,7 @@ export class AuthService {
       msg: ' I have signed in',
     };
   }
-  signup() {
+  signup(dto: AuthDto) {
     return {
       msg: ' I have signed up',
     };
