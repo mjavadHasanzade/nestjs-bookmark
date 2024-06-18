a518cf446a27da9f94b37a02dab525c0d20826ab
mjavadHasanzade <hjavad522@gmail.com>
2024-06-18 10:24:24 +0330

    feat: add custom decoator to get user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators';
 import { JwtGuard } from 'src/auth/guards';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  getMe(@Req() req) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
